688959737189c1065cebdba75db7deaac35fd0b6
wusung <twkoci@gmail.com>
2023-12-08 00:20:43 +0800

    chore: initial commit

---
+++ installer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+mkdir -p ~/.mytb-edge-logs && sudo chown -R 799:799 ~/.mytb-edge-logs
+mkdir -p ~/.mytb-edge-data && sudo chown -R 799:799 ~/.mytb-edge-data
+
+
+mkdir -p ~/.mytb-data && sudo chown -R 799:799 ~/.mytb-data
+mkdir -p ~/.mytb-logs && sudo chown -R 799:799 ~/.mytb-logs
