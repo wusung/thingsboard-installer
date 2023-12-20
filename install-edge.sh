1474f2558d69dd6180a9738e2e854954f49dc7f9
Wusung Peng <twkoci@gmail.com>
2023-12-20 21:19:51 +0800

    Update install-edge.sh

--- install-edge.sh
+++ install-edge.sh
@@ -3,6 +3,8 @@
 mkdir -p ~/.mytb-edge-logs && sudo chown -R 799:799 ~/.mytb-edge-logs
 mkdir -p ~/.mytb-edge-data && sudo chown -R 799:799 ~/.mytb-edge-data
 
-cd thingsboard-edge/
+WORK_DIR=~/.local/thingsboard-edge
+git clone https://github.com/wusung/thingsboard-installer.git $WORK_DIR
+cd $WORK_DIR/thingsboard-edge
 docker compose up -d
 cd -
