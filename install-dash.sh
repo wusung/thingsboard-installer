0f79eafba10f3aebd225767844968b315678c00e
Wusung Peng <twkoci@gmail.com>
2023-12-20 21:19:14 +0800

    Update install-dash.sh

--- install-dash.sh
+++ install-dash.sh
@@ -3,8 +3,9 @@
 mkdir -p ~/.mytb-data && sudo chown -R 799:799 ~/.mytb-data
 mkdir -p ~/.mytb-logs && sudo chown -R 799:799 ~/.mytb-logs
 
-WORK_DIR=~/.local/thingsboard
+
+WORK_DIR=~/.local/thingsboard-dash
 git clone https://github.com/wusung/thingsboard-installer.git $WORK_DIR
-cd $WORK_DIR/thingsboard-edge
+cd $WORK_DIR/thingsboard-dash
 docker compose up -d
 cd -
