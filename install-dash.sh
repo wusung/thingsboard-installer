24f0ae6604ef8f33e167e2fd6f2b5146d4e96b7e
Wusung Peng <twkoci@gmail.com>
2024-01-23 22:38:12 +0800

    Update install-dash.sh

--- install-dash.sh
+++ install-dash.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo apt install -y docker.io docker-compose-v2
+sudo usermod -aG docker $USER
+
 mkdir -p ~/.mytb-data && sudo chown -R 799:799 ~/.mytb-data
 mkdir -p ~/.mytb-logs && sudo chown -R 799:799 ~/.mytb-logs
 
